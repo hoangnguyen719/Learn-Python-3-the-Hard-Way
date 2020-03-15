8abe5748ab1c0d28dbcaa9a81eb9daba2f878035
Hoang Nguyen <hoang.m.nguyen719@gmail.com>
2020-03-14 22:38:57 -0700

    created tarball for PythonJungle :wq

--- ex41-50/ex46/setup.py
+++ ex41-50/ex46/setup.py
@@ -4,14 +4,14 @@ except ImportError:
     from distutils.core import setup
 
 config = {
-    'name': 'projectname',
+    'name': 'PythonJungle',
     'description': 'This package is copied from ex43_owned and meant for ex46 - LP3THW',
     'version': '0.1',
     'packages': ['PythonJungle'],
     'author': 'Hoang Nguyen',
     'url': 'URL to get it at',
     'download_url': 'Where to download it.',
-    'author_email': 'My email.',
+    'author_email': 'hoang.m.nguyen719@gmail.com',
     'install_requires': ['nose'],
     'scripts': []
 }
