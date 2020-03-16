974320eca92ff3649c4a66e6f99922ab824e6ed9
Hoang Nguyen <hoang.m.nguyen719@gmail.com>
2020-03-15 22:49:32 -0700

    finished ex47

--- ex41-50/ex47/ex47/game.py
+++ ex41-50/ex47/ex47/game.py
@@ -1,11 +1,15 @@
 class Room(object):
-    def __init__(self, name, description):
+    def __init__(self, name, description, password=None):
         self.name = name
         self.description = description
+        self.password = password
         self.paths = {}
 
-    def go(self, direction):
-        return self.paths.get(direction, None)
+    def go(self, direction, password=None):
+        if self.paths.get(direction).password == password:
+            return self.paths.get(direction, None)
+        else:
+            return self
 
     def add_paths(self, path):
-        self.paths.update(paths)
+        self.paths.update(path)
