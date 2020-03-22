cdf69dfea2717755f58a3e94d83f2114ee7cbb38
Hoang Nguyen <hoang.m.nguyen719@gmail.com>
2020-03-21 21:21:20 -0700

    sdist ex48

--- ex41-50/ex48/setup.py
+++ ex41-50/ex48/setup.py
@@ -4,10 +4,10 @@ except ImportError:
     from distutils.core import setup
 
 config = {
-    'name': 'NAME',
+    'name': 'ex48',
     'description': 'Description',
     'version': '0.1',
-    'packages': ['NAME'],
+    'packages': ['ex48'],
     'author': 'Hoang Nguyen',
     'author_email': 'hoang.m.nguyen719@gmail.com',
     'install_requires': [
