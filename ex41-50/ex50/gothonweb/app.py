925fd9615a8419925ee2edb07efb0fcaeac935a1
Hoang Nguyen <hoang.m.nguyen719@gmail.com>
2020-03-23 02:14:31 -0700

    add things to ex50/tests/foo.html

--- ex41-50/ex50/gothonweb/app.py
+++ ex41-50/ex50/gothonweb/app.py
@@ -7,5 +7,9 @@ def index():
     greeting = "Hello World"
     return render_template("index.html", greeting=greeting)
 
+@app.route('/1')
+def index2():
+    return render_template("foo.html")
+    
 if __name__ == "__main__":
     app.run()
